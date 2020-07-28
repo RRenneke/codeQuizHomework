acbc3c988bbe8cdb2a4ec498f0e41d29e41e6ec2
Robin Renneke <rennekerobin@yahoo.com>
2020-07-27 21:06:58 -0500

    questions added

--- script.js
+++ script.js
@@ -1,43 +1,131 @@
-var startButton = document.getElementById("startBtn")
-var questionContainerElement = document.getElementById ("questionContainer")
-var questionElement = document.getElementById("question")
-var answerButtonElement = document.getElementById ("answerButtons")
+const startButton = document.getElementById("startBtn")
+const nextButton = document.getElementById("nextBtn")
+const questionContainerElement = document.getElementById("questionContainer")
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById ("answerButtons")
 
-let shuffledQuestion, currentQuestion
+let shuffledQuestions, currentQuestionIndex
 
-startButton.addEventListener("click",startGame)
+startButton.addEventListener("click", startGame)
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame(){
-    console.log ("Started")
-    startButton.classList.add("hide")
-    shuffledQuestion = questions.sort(() => Math.random() - .5)
-    currentQuestion = 0
-    questionContainerElement.classList.remove("hide")
-    nextQuestion()
+  startButton.classList.add("hide")
+  shuffledQuestions = questions.sort(() => Math.random()- .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove("hide")
+  setNextQuestion()
 }
 
-function nextQuestion(){
-    showQuestion(shuffledQuestion[currentQuestion])
 
+function setNextQuestion(){
+    resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question){
-    questionElement.innerText= questions.question
+ questionElement.innerText=question.question
+ question.answers.forEach(answer => {
+    const button = document.createElement("button")
+    button.innerText = answer.text
+    button.classList.add("btn")
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListener("click", selectAnswer)
+    answerButtonsElement.appendChild(button)
+ })
 }
 
-function selectAnswer(){
-
+function resetState(){
+    clearStatusClass(document.body)
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild){
+        answerButtonsElement.removeChild (answerButtonsElement.firstChild)
+    }
 }
 
-var questions = [
-        {
-        question:"What is 2+2?",
-        answer: 
+function selectAnswer(e){
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if(shuffledQuestions.length > currentQuestionIndex + 1)
+    nextButton.classList.remove("hide")
+    else{
+        startButton.innerText = "Restart"
+        startButton.classList.remove("hide")
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element){
+    element.classList.remove("correct")
+    element.classList.remove ("wrong")
+}
+
+const questions = [
+      {
+        question:"What does 3M stand for?",
+        answers: 
             [
-            {text: "4", correct: true},
-            {text: "0", correct: false},
-            {text: "7", correct: false},
-            {text: "90", correct: false}
+            {text: "Minnesota Minning & Manufacturing", correct: true},
+            {text: "Money Market Manager", correct: false},
+            {text: "Mickey Mouse's Mansion", correct: false},
+            {text: "Mars Man Mission", correct: false}
             ]
-        }
+        },
+        {
+            question:"What is 10 - 5?",
+            answers: 
+                [
+                {text: "0", correct: false},
+                {text: "7", correct: false},
+                {text: "5", correct: true},
+                {text: "90", correct: false}
+                ]
+            },
+            {
+                question:"Which is not a lipid?",
+                answers: 
+                    [
+                    {text: "HDL", correct: false},
+                    {text: "LDL", correct: false},
+                    {text: "Trygliceride", correct: true},
+                    {text: "FDL", correct: false}
+                    ]
+                },
+                {
+                    question:"What does a dog say?",
+                    answers: 
+                        [
+                        {text: "Meow", correct: false},
+                        {text: "Howdy", correct: false},
+                        {text: "Woof", correct: true},
+                        {text: "Shhh", correct: false}
+                        ]
+                    },
+                    {
+                        question:"What state is most North?",
+                        answers: 
+                            [
+                            {text: "Florida", correct: false},
+                            {text: "Texas", correct: false},
+                            {text: "South Dakota", correct: true},
+                            {text: "North Carolina", correct: false}
+                            ]
+                        }
     ]
