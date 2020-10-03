4be0eb3944219ab89f4a4cb23cb0c1d3c1df0e20
Robin Renneke <rennekerobin@yahoo.com>
2020-10-02 19:10:00 -0500

    updated HTML and added vars to js

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-//create a time variable using CSS selector
-var timeElement = document.querySelector(".time");
+ //object with questions, choices, and answer. Class actvity 3.33 shows an example.
+ //questions from https://jaxenter.com/know-your-javascript-trivia-134924.html
+ var quizQuestions = [
+    {question : "1.) What is Javascript?",
+    choiceA : "A programming language",
+    choiceB : "An organic Columbian coffee",
+    choiceC : "A means to write a play",
+    choiceD: "A childs toy, like a pogo stick",
+    correctAnswer : "a"},
+    
+    {question : "2.) Who created JavaScript?", 
+    choiceA : "Microsoft", 
+    choiceB : "Netscape",
+    choiceC : "Microsystems",
+    choiceD: "Apple",
+    correctAnswer : "b"},
+  
+    {question : "3.) Is JavaScript a front-end, back-end, or full-stack programming language?",
+    choiceA : "Full Stack Programming",
+    choiceB : "Back-end Programming",
+    choiceC : "Front-end Programming",
+    choiceD: "None, it's for styling",
+    correctAnswer : "a"},
+  
+    {question : "4.) Which of the following is not a reserved word in JavaScript?", 
+    choiceA : "default",
+    choiceB : "finally",      
+    choiceC : "undefined",
+    choiceD: "touch",
+    correctAnswer: "c"},
+  ];
 
-//create variable with number of seconds user has to start
-var secondsRemaining = 10;
+//HTML elements to edit the DOM: the four differnt page layouts
+var homePageEl = document.getElementById("homePage");
+var questionsPageEl = document.getElementById("questionsPage");
+var gameOverPageEl = document.getElementById("gameOverPage");
+var scoresPageEl = document.getElementById("scoresPage");
 
-function setTime() {
-  //put before increment starts to show user they start with 60 seconds
-  timeElement.textContent = "You have " + secondsRemaining + " seconds remaining.";  
-  //setup variable that will show incremented seconds
-  var timerInterval = setInterval(function() { 
-  //secondsRemaining-- will take one second off at a time
-  secondsRemaining--;
-  //Content to show user
-    timeElement.textContent = "You have " + secondsRemaining + " seconds remaining.";  
-//this will stop the timer from running if it reaches 0 and alert the user
-    if(secondsRemaining === 0) {
-      clearInterval(timerInterval);
-      alert("Sorry! Time is up")
-    }
-//1,000 is milliseconds, 1 equals one second
-  }, 1000);
-}
-//call the function
-setTime();
+//HTML elements to edit the DOM: buttons
+var startBtn = document.getElementById("startBtn");
+var aBtn = document.getElementById("a");
+var bBtn = document.getElementById("b");
+var cBtn = document.getElementById("c");
+var dBtn = document.getElementById("d");
+var submitBtn = document.getElementById("submitScoreBtn");
+var againClearBtns = document.getElementById("againClearBtns");
+
+//HTML elements to edit the DOM: remaining ones
+var questionsEl = document.getElementById("questions");
+var resultsEl = document.getElementById("answerResult");
+var timerEl = document.getElementById("timer");
+var userNameEl = document.getElementById("userName");
+var scoresTableEl = document.getElementById("highScoreTable");
+var highScoreNameEl = document.getElementById("highScoreName");
+var highScoreScoreEl = document.getElementById("highScoreScore");
